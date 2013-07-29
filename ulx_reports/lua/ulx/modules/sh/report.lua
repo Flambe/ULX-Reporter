c3af5cbce82676f5fb141d4df64d5a5b9d542bea
Steve <steve.axtmann+github@gmail.com>
2013-07-30 00:22:52 +0100

    began work on the xgui part which shows the reports to admins

--- ulx_reports/lua/ulx/modules/sh/report.lua
+++ ulx_reports/lua/ulx/modules/sh/report.lua
@@ -2,7 +2,7 @@ function ulx.report(plr, string)
 
 end
 local report = ulx.command( "Utility", "ulx report", ulx.report, "!report")
-report:addParam{ type=ULib.cmds.StringArg, hint="infop", ULib.cmds.takeRestOfLine }
+report:addParam{ type=ULib.cmds.StringArg, hint="info", ULib.cmds.takeRestOfLine }
 report:defaultAccess(ULib.ACCESS_ALL)
 report:help( "Report players who are disobeying the rules." )
 report:logString( "#1s sent a report")
---
+++ ulx_reports/lua/ulx/xgui/reports.lua
@@ -0,0 +1,4 @@
+--Reports module v2 for ULX GUI by Flambe
+--Shows reported content to those with permission to see it
+
+local reports = xlib.makepanel{ parent=xgui.null }
